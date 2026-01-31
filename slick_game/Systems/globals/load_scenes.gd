986429beebca5571014602975b46b42b85abe333
VnEagle <129601778+VnEagle@users.noreply.github.com>
2026-01-31 18:16:00 +0200

    Added Player Changes and other fixes
    - Corrected the paths to the scene files
    - Added crouching
    - Added sliding
    - Added separate hitbox for when crouching
    
    (Note: The game can't capture the mouse while the pause menu is visible in the player scene. Needs to be fixed)

--- slick_game/Systems/globals/load_scenes.gd
+++ slick_game/Systems/globals/load_scenes.gd
@@ -1,14 +1,14 @@
 class_name loadScenes
 extends Node
 
-@onready var settings_menu = preload("res://Scenes/before_game_settings_menu.tscn")
-@onready var main_menu = preload("res://Scenes/main_menu.tscn")
+@onready var settings_menu = preload("res://Scenes/Menus/before_game_settings_menu.tscn")
+@onready var main_menu = preload("res://Scenes/Menus/main_menu.tscn")
 
 func change_to_settings():
-	get_tree().change_scene_to_file("res://Scenes/settings_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menus/settings_menu.tscn")
 
 func change_to_main_menu():
-	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
 
 func start_game(scene: PackedScene):
 	get_tree().change_scene_to_packed(scene)
