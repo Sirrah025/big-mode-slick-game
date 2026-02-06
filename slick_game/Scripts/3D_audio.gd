d4c05c6cdf0e2335ca31ed5759effac9256ec5ef
William Harris <william.harris108@gmail.com>
2026-02-06 14:27:44 -0600

    Refining random audio play, avoiding logic errors and endless loops

--- slick_game/Scripts/3D_audio.gd
+++ slick_game/Scripts/3D_audio.gd
@@ -3,6 +3,8 @@ extends AudioStreamPlayer3D
 @export var audio: Array[AudioStreamMP3]
 
 @export var truncate_audio = false
+
+var previous_index := -1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,6 +19,11 @@ func _process(delta: float) -> void:
 
 func play_random_audio() -> void:
 	if !playing:
-		var random_audio = randi_range(0, len(audio)-1)
-		stream = random_audio
+		var random_audio
+		while true:
+			random_audio = randi_range(0, len(audio)-1)
+			if random_audio != previous_index or len(audio) == 1:
+				break
+		previous_index = random_audio
+		stream = audio.get(random_audio)
 		play()
